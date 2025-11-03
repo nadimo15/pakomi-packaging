eb63098bf064a464d6e9bbffb2ce274678477ad1
nadimo15 <nadimonano@gmail.com>
2025-11-03 12:52:09 +0100

    fix(frontend build): include shared root files and add re-export shims; relax TS flags

---
+++ frontend/src/components/LanguageSwitcher.tsx
@@ -0,0 +1,2 @@
+export { default } from '../../../components/LanguageSwitcher.tsx';
+export * from '../../../components/LanguageSwitcher.tsx';
---
+++ frontend/src/components/admin/ImageUploader.tsx
@@ -0,0 +1,2 @@
+export { default } from '../../../../components/admin/ImageUploader.tsx';
+export * from '../../../../components/admin/ImageUploader.tsx';
